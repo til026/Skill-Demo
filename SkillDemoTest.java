927e239250c6d1fc859781f296c03cd24a0128c5
til026 <97641884+til026@users.noreply.github.com>
2022-02-03 16:20:46 -0800

    right one

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void addition() {
-        assertEquals(2, SkillDemo.add(2, 4));
+        assertEquals(2, SkillDemo.add(2, 0));
     }
 }
